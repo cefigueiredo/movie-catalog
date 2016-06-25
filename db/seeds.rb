f2184347a26b408bf5d3cc0a5e6eadc752321acd
Carlos Figueiredo <carlos.figueiredo87@gmail.com>
2016-06-25 16:15:34 -0300

    Create a seed to populate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def movie_url(partial_id)
+  "http://www.omdbapi.com/?i=tt227#{partial_id}&plot=short&r=json"
+end
+
+def get_movie(url)
+  res = Net::HTTP.get URI(url)
+  JSON.parse res
+end
+
+def to_date(date_string)
+  return if date_string == "N/A"
+
+  Date.strptime(date_string, "%d %b %Y")
+end
+
+def populate(from, to)
+  (from..to).each do |n|
+    puts "#{n} - From: #{from} To: #{to}"
+
+    @from = n + 1
+    url = movie_url(n)
+    movie = get_movie url
+    imdb_id = url.slice!(/tt[0-9]+/)
+
+    if movie["Response"] == "True"
+      Movie.create({
+        title: movie["Title"],
+        storyline: movie["Plot"],
+        imdb_link: "http://www.imdb.com/title/#{imdb_id}",
+        release_date: to_date(movie["Released"]),
+        genre_list: movie["Genre"]
+      })
+    end
+  end
+rescue Net::OpenTimeout => error
+  puts "Rescuing of: #{error.message}"
+  populate @from, ENV["seed_to"].to_i
+end
+
+populate(ENV["seed_from"].to_i, ENV["seed_to"].to_i)
